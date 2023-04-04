2320fd07291adb2a75901a5365cf5f709ca43764
Drame Success <d.ije95@gmail.com>
2023-04-04 20:45:28 +0100

    updating codebase for live

--- db/index.js
+++ db/index.js
@@ -1,6 +1,7 @@
+
 const knexConfig = require('./knexfile');
 //initialize knex
-const knexDb = require('knex')(knexConfig[process.env.DB_CONN_ENV_LIVE || "live"])
+const knexDb = require('knex')(knexConfig[process.env.DB_CONN_ENV_LIVE || "production"])
 // const knexDb = require('knex')(knexConfig[process.env.DB_CONN_ENV_STAGING || "staging"])
 // const knexDb = require('knex')(knexConfig[process.env.DB_CONN_ENV || "development"])
 
