b1f90dcc86997bba71a6cc80e643eab92b868ee4
root <root@ip-172-31-90-34.ec2.internal>
2023-04-04 21:44:20 +0000

    new from server

--- db/index.js
+++ db/index.js
@@ -1,8 +1,8 @@
 
 const knexConfig = require('./knexfile');
 //initialize knex
-// const knexDb = require('knex')(knexConfig[process.env.DB_CONN_ENV_LIVE || "production"])
+ const knexDb = require('knex')(knexConfig[process.env.DB_CONN_ENV_LIVE || "production"])
 // const knexDb = require('knex')(knexConfig[process.env.DB_CONN_ENV_STAGING || "staging"])
-const knexDb = require('knex')(knexConfig[process.env.DB_CONN_ENV_DEV || "development"])
+//const knexDb = require('knex')(knexConfig[process.env.DB_CONN_ENV_DEV || "development"])
 
 module.exports = knexDb;
